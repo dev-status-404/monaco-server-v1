3b761ade5cebe8d0d3edcc711d3defe0d29313f9
Dev-404 <91515695+asadtanvirshami@users.noreply.github.com>
2026-04-11 00:54:13 +0500

    push

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -36,29 +36,24 @@ const updateGame = async (data) => {
 };
 
 const getGame = async (q) => {
-  const {
-    id,
-    name,
-    status,
-    page,
-    limit,    
-  } = q;
+  const { id, name, status, page, limit } = q;
 
-    const where = {};
+  const where = {};
 
   if (id) where.id = id;
   if (name) where.name = name;
   if (status) where.status = status;
+  if (!status) where.status = "active";
 
-    const pageNumber = Number(page) || 1;
+  const pageNumber = Number(page) || 1;
   const pageLimit = Number(limit) || 10;
   const offset = (pageNumber - 1) * pageLimit;
 
-    const { rows: game, count } = await Game.findAndCountAll({
-      where,
-      offset,
-      limit: pageLimit,
-    });
+  const { rows: game, count } = await Game.findAndCountAll({
+    where,
+    offset,
+    limit: pageLimit,
+  });
 
   return {
     success: true,
@@ -73,7 +68,6 @@ const getGame = async (q) => {
     code: 200,
   };
 };
-    
 
 const deleteGame = async (id) => {
   const game = await Game.destroy({ where: { id } });
@@ -86,7 +80,6 @@ const deleteGame = async (id) => {
     code: 200,
   };
 };
-
 
 const bulkDeleteGame = async (ids) => {
   const game = await Game.destroy({
